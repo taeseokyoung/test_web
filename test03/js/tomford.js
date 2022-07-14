3b068ef82a488f9c6250ae3b9879b63e8b3fc4ed
taeseokyoung <xotjrud@gmail.com>
2022-07-14 17:57:12 +0900

    20220714

--- test03/js/tomford.js
+++ test03/js/tomford.js
@@ -3,10 +3,10 @@ $(function () {
     $(window).scroll(function () {
         var sct = $(window).scrollTop();
         if (sct > 10) {
-            $('.header_fixed').addClass('on')
+            $('.title').addClass('on')
         }
         else {
-            $('.header_fixed').removeClass('on')
+            $('.title').removeClass('on')
         }
 
     })
@@ -14,6 +14,16 @@ $(function () {
     $('.main_video').YTPlayer({
         videoURL: 'https://www.youtube.com/watch?v=7F5CbEw5bXE',
         containment: '.main_visual',
+        autoPlay: true,
+        mute: true,
+        showControls: false,
+        // 스크롤 내려가서 동영상이 보일 때 쯤 재생해라
+        playOnlyIfVisible: true,
+    });
+
+    $('.main_product .product_img figure:last-child').YTPlayer({
+        videoURL: 'https://www.youtube.com/watch?v=zWiwPt9-6Us',
+        containment: 'figure:last-child',
         autoPlay: true,
         mute: true,
         showControls: false,
