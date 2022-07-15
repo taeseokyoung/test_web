9fd451fd3f0c5e356f0232874f36247b811c7c38
taeseokyoung <xotjrud@gmail.com>
2022-07-15 13:28:24 +0900

    20220715

--- test03/js/tomford.js
+++ test03/js/tomford.js
@@ -1,15 +1,8 @@
 $(function () {
-
-    $(window).scroll(function () {
-        var sct = $(window).scrollTop();
-        if (sct > 10) {
-            $('.title').addClass('on')
-        }
-        else {
-            $('.title').removeClass('on')
-        }
-
-    })
+    $('.popUp i').on('click', function (e) {
+        e.preventDefault();
+        $('.popUp').hide();
+    });
 
     $('.main_video').YTPlayer({
         videoURL: 'https://www.youtube.com/watch?v=7F5CbEw5bXE',
@@ -21,14 +14,43 @@ $(function () {
         playOnlyIfVisible: true,
     });
 
-    $('.main_product .product_img figure:last-child').YTPlayer({
-        videoURL: 'https://www.youtube.com/watch?v=zWiwPt9-6Us',
-        containment: 'figure:last-child',
+    $('.main_product .product_img #video').YTPlayer({
+        videoURL: 'https://youtu.be/zWiwPt9-6Us',
+        containment: '#video',
         autoPlay: true,
         mute: true,
         showControls: false,
-        // 스크롤 내려가서 동영상이 보일 때 쯤 재생해라
         playOnlyIfVisible: true,
     });
+
+    $('.toTop').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 200)
+    });
+
+    $(window).scroll(function () {
+        var sct = $(window).scrollTop();
+        if (sct > 10) {
+            $('.title').addClass('on')
+        }
+        else {
+            $('.title').removeClass('on')
+        };
+
+        if (sct > 500) {
+            $('.toTop').fadeIn(300)
+        }
+        else {
+            $('.toTop').fadeOut(1000)
+        }
+    });
+
+    $('.main_slogan .perfume_img').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 700,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+    });
+
 })
 
