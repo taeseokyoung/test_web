a1bdc75e2f2f2bfb81e55a4974894c6fc2976ace
taeseokyoung <xotjrud@gmail.com>
2022-07-25 17:44:35 +0900

    20220725

---
+++ test04/js/main.js
@@ -0,0 +1,9 @@
+// $('.main_video').YTPlayer({
+//     videoURL: 'https://www.youtube.com/watch?v=rlJm_DCmRfo',
+//     containment: '.main_visual',
+//     autoPlay: true,
+//     mute: true,
+//     showControls: false,
+//     // 스크롤 내려가서 동영상이 보일 때 쯤 재생해라
+//     playOnlyIfVisible: true,
+// });
