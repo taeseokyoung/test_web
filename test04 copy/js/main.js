172905df5aa84ceadf5b180530d72b15e751b6a4
taeseokyoung <105770887+taeseokyoung@users.noreply.github.com>
2022-07-26 03:29:14 +0900

    Add files via upload

---
+++ test04 copy/js/main.js
@@ -0,0 +1,9 @@
+$('.main_video').YTPlayer({
+    videoURL: 'https://youtu.be/nnVOr1CzozY',
+    containment: '.main_visual',
+    autoPlay: true,
+    mute: true,
+    showControls: false,
+    // 스크롤 내려가서 동영상이 보일 때 쯤 재생해라
+    playOnlyIfVisible: true,
+});
