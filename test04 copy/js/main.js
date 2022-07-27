8978df49421b09e6de093aed2ab93f80af41c423
taeseokyoung <xotjrud@gmail.com>
2022-07-27 17:50:42 +0900

    20220727

--- test04 copy/js/main.js
+++ test04 copy/js/main.js
@@ -9,5 +9,32 @@ $(function () {
         playOnlyIfVisible: true,
     });
 
-    $('.banner_slider').slick();
+    $('.new_popup .new_tap li').on('click', function (e) {
+        e.preventDefault();
+        var idx = $(this).index();
+        $('.new_popup .bottom>div').removeClass('on')
+        $('.new_popup .bottom>div').eq(idx).addClass('on')
+    })
+
+
+    $('.banner_slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+    });
+
+    $('').on('click', function () {
+
+    })
 })
+
+
+// $('.play').click(function () {
+//     $('.visual').slick('slickPlay');
+// });
+
+// $('.stop').click(function () {
+//     $('.visual').slick('slickPause');
+// });
