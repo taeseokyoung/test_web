01c002382dc2a7a246f0166d0d5069bf557152db
taeseokyoung <xotjrud@gmail.com>
2022-08-04 17:49:16 +0900

    20220804

--- test05/js/main.js
+++ test05/js/main.js
@@ -1,7 +1,35 @@
 $(function () {
-    $('.main_slider').slick({
+    //     $('.main_slider').slick({
+    //         arrows: false,
+    //         dots: true,
+    //     });
+
+    $('.match_slider .slick').slick({
+        centerMode: true,
         arrows: false,
-        dots: true,
+        centerPadding: '30px',
+        variableWidth: true,
+        slidesToShow: 3
+        // responsive: [
+        //     {
+        //         breakpoint: 768,
+        //         settings: {
+        //             arrows: false,
+        //             centerMode: true,
+        //             centerPadding: '40px',
+        //             slidesToShow: 3
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             arrows: false,
+        //             centerMode: true,
+        //             centerPadding: '40px',
+        //             slidesToShow: 1
+        //         }
+        //     }
+        // ]
     });
 })
 
