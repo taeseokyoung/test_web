0665a31b6be0d239dd8355034d8ba8fe873e01c6
taeseokyoung <xotjrud@gmail.com>
2022-08-05 14:01:43 +0900

    20220805

--- test05/js/main.js
+++ test05/js/main.js
@@ -1,8 +1,9 @@
 $(function () {
-    //     $('.main_slider').slick({
-    //         arrows: false,
-    //         dots: true,
-    //     });
+
+    $('.match_slider .slick').on('init afterchange', function (e, s, c) {
+        var current = $('.slick_center');
+        current.addClass('on').siblings().removeClass('on');
+    });
 
     $('.match_slider .slick').slick({
         centerMode: true,
@@ -10,28 +11,48 @@ $(function () {
         centerPadding: '30px',
         variableWidth: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
-        // responsive: [
-        //     {
-        //         breakpoint: 768,
-        //         settings: {
-        //             arrows: false,
-        //             centerMode: true,
-        //             centerPadding: '40px',
-        //             slidesToShow: 3
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 480,
-        //         settings: {
-        //             arrows: false,
-        //             centerMode: true,
-        //             centerPadding: '40px',
-        //             slidesToShow: 1
-        //         }
-        //     }
-        // ]
     });
-})
+
+    $('.main_slider').slick({
+        arrows: false,
+        dots: true,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+
+    });
+
+    $('.txt_slider').slick({
+        vertical: true,
+        arrows: false,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        autoplay: true,
+        autoplaySpeed: 1300,
+
+
+        // verticalSwiping: true,
+    });
+
+    $('.highlight .slider_for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider_nav',
+    });
+
+    $('.highlight .slider_nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider_for',
+        arrows: false,
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
+});
 
 
