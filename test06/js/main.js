f60223fc1c688ed09f05b78d8d5ae6f8b642f499
taeseokyoung <xotjrud@gmail.com>
2022-08-11 17:52:15 +0900

    20220811

---
+++ test06/js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+
+    var mainSwiper = new Swiper('.swiper-container', {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 30,
+        centeredSlides: true,
+        center
+        // pagination: {
+        //     el: 'swiper-pagination',
+        // },
+
+        // navigation: {
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev',
+        // },
+
+    });
+})
+
