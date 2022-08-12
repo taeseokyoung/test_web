975067be770ca68640d709b6ecaadb5239d1f1d7
taeseokyoung <xotjrud@gmail.com>
2022-08-12 17:43:00 +0900

    20220812

--- test06/js/main.js
+++ test06/js/main.js
@@ -1,20 +1,28 @@
 $(function () {
-
-    var mainSwiper = new Swiper('.swiper-container', {
-        loop: true,
-        slidesPerView: 1,
-        spaceBetween: 30,
-        centeredSlides: true,
-        center
-        // pagination: {
-        //     el: 'swiper-pagination',
-        // },
-
-        // navigation: {
-        //     nextEl: '.swiper-button-next',
-        //     prevEl: '.swiper-button-prev',
-        // },
+    $('.mainSlider').slick({
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        centerMode: true,
+        centerPadding: '350px',
+        slidesToShow: 1,
+        pauseOnHover: false,
 
     });
-})
 
+    $('.mainSlider').on('afterChange', function (e, s, c) {
+        $('.gnb>ul>li').eq(c).addClass('on').siblings().removeClass('on')
+
+        // if ($('.mainSlider figure').hasClass('slick-center')) {
+        //     var idx = $(this).index();
+
+        //     $('.gnb li').eq(idx).addClass('on').siblings().removeClass('on')
+        // }
+    });
+
+
+
+
+
+})
