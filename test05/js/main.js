bf2ab662c29d56fdb761133bf90f759f598c7aaf
taeseokyoung <xotjrud@gmail.com>
2022-08-19 14:41:35 +0900

    20220819

--- test05/js/main.js
+++ test05/js/main.js
@@ -1,36 +1,99 @@
 $(function () {
+    $('.mainSlider').slick({
+        // arrows: false,
+        // dots: false,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        // centerMode: true,
+        // centerPadding: '350px',
+        // slidesToShow: 1,
+        // pauseOnHover: false,
 
-    var mainSwiper = new Swiper('.swiper-container', {
-        speed: 400,
-        // spaceBetween: 100,
-        initialSlide: 0,
-        //truewrapper adoptsheight of active slide
-        autoHeight: false,
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        // delay between transitions in ms
-        autoplay: 1000,
-        autoplayStopOnLast: false, // loop false also
-        // If we need pagination
-        pagination: '.swiper-pagination',
-        paginationType: "bullets",
-
-        // Navigation arrows
-        // nextButton: '.swiper-button-next',
-        // prevButton: '.swiper-button-prev',
-
-        // And if we need scrollbar
-        //scrollbar: '.swiper-scrollbar',
-        // "slide", "fade", "cube", "coverflow" or "flip"
-        effect: 'slide',
-        // Distance between slides in px.
-        spaceBetween: 30,
-        slidesPerView: 2,
-        centeredSlides: true,
-        slidesOffsetBefore: 0,
-        grabCursor: true,
+        // slick Variable Width
+        dots: false,
+        arrows: false,
+        // infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        // speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
     });
+
+    $('.mainSlider').on('afterChange', function (e, s, c) {
+        $('.gnb>ul>li').eq(c).addClass('on').siblings().removeClass('on')
+
+        // if ($('.mainSlider figure').hasClass('slick-center')) {
+        //     var idx = $(this).index();
+
+        //     $('.gnb li').eq(idx).addClass('on').siblings().removeClass('on')
+        // }
+    });
+
+
+    $('.miniSlider').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 2,
+    });
+
+    $('.leftSlider').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 1,
+        vertical: true,
+    });
+
+    $('.leftSlider').on('afterChange', function (e, s, c) {
+        console.log(c);
+        $('.rightSlider .slide').removeClass('on');
+        $('.rightSlider .slide').eq(c).addClass('on');
+
+    })
+
+    $('.leftArrows i:nth-child(1)').on('click', function (e) {
+        e.preventDefault();
+        $('.leftSlider').slick('slickPrev');
+    });
+
+    $('.leftArrows i:nth-child(2)').on('click', function (e) {
+        e.preventDefault();
+        $('.leftSlider').slick('slickNext');
+    });
+
+
+    $('.rightSlider .slide').slick({
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        slidesToShow: 5,
+        touchMove: true
+    });
+
 })
 
-// swiper 참고 링크 https://codepen.io/dannyb/pen/mVZbar
+
+
+// $('.gnb .mainmenu>ul>li').on('hover', function () {
+//     var idx = $(this).index();
+
+//     $('.gnb .multimenu .container').eq(idx).addClass('on').siblings().removeClass('on');
+
+
+// })
+
+// $('.gnb .mainmenu>ul>li').onMouseOver({
+
+// })
+
+
+       //    $('.gnb>ul>li>a').on('click', function (e) {
+        //             if ($('.gnb').hasClass('on')) {
+        //                 e.preventDefault();
+        //                 $('.sub_menu').slideUp();
+        //                 $(this).next().slideToggle();
+        //             }
+
+        //         });
