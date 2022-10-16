a94a377167689dfb6b18414f7c5df0761d0f6b66
taeseokyoung <xotjrud@gmail.com>
2022-10-17 01:29:33 +0900

    20221017 수정시작

---
+++ test06/js/main.js
@@ -0,0 +1,100 @@
+$(function () {
+    $('.mainSlider').slick({
+        // arrows: false,
+        // dots: false,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        // centerMode: true,
+        // centerPadding: '350px',
+        // slidesToShow: 1,
+        // pauseOnHover: false,
+
+        // slick Variable Width
+        dots: false,
+        arrows: false,
+        // infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        // speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+    });
+
+    $('.mainSlider').on('afterChange', function (e, s, c) {
+        $('.Gnb .menu .tit').removeClass('on');
+        $('.Gnb .menu .tit').eq(c).addClass('on');
+
+        // if ($('.mainSlider figure').hasClass('slick-center')) {
+        //     var idx = $(this).index();
+
+        //     $('.gnb li').eq(idx).addClass('on').siblings().removeClass('on')
+        // }
+    });
+
+
+    $('.miniSlider').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 2,
+    });
+
+    $('.leftSlider').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 1,
+        vertical: true,
+    });
+
+    $('.leftSlider').on('afterChange', function (e, s, c) {
+        console.log(c);
+        $('.rightSlider .slide').removeClass('on');
+        $('.rightSlider .slide').eq(c).addClass('on');
+
+    })
+
+    $('.leftArrows i:nth-child(1)').on('click', function (e) {
+        e.preventDefault();
+        $('.leftSlider').slick('slickPrev');
+    });
+
+    $('.leftArrows i:nth-child(2)').on('click', function (e) {
+        e.preventDefault();
+        $('.leftSlider').slick('slickNext');
+    });
+
+
+    $('.rightSlider .slide').slick({
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        slidesToShow: 5,
+        touchMove: true
+    });
+
+})
+
+
+
+// $('.gnb .mainmenu>ul>li').on('hover', function () {
+//     var idx = $(this).index();
+
+//     $('.gnb .multimenu .container').eq(idx).addClass('on').siblings().removeClass('on');
+
+
+// })
+
+// $('.gnb .mainmenu>ul>li').onMouseOver({
+
+// })
+
+
+       //    $('.gnb>ul>li>a').on('click', function (e) {
+        //             if ($('.gnb').hasClass('on')) {
+        //                 e.preventDefault();
+        //                 $('.sub_menu').slideUp();
+        //                 $(this).next().slideToggle();
+        //             }
+
+        //         });
